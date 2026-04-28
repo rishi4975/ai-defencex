f6f56eac7fa496fe432ed70a520288bb76fdbd8c
Rushikesh Babhulkar <rushikeshbabhulkar09@gmail.com>
2026-04-29 01:00:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("AIdefenceX Cyber Threat Detection Started")
+
+# Example Random Forest placeholder
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier()
+
+print("Model Loaded Successfully")
